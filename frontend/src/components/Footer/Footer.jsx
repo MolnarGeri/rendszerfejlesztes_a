3ac33de102413cf037c31794c6a56fc16964870c
MolnarGeri <164872999+MolnarGeri@users.noreply.github.com>
2024-12-04 19:28:45 +0100

    Update Footer.jsx
    Kivéve a social media linkek és kivéve az semmire nem mutató linkek a footerből

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -9,21 +9,9 @@ const Footer = () => {
         <div className="footer-content-left">
             <img src={assets.logo} alt="" />
             <p>A NutriGo-val az egészséges életmód még sosem volt ilyen könnyen elérhető! Küldetésünk, hogy friss, tápláló és ízletes ételeket szállítsunk Önnek, legyen szó ebédről, vacsoráról vagy akár egy gyors nassolnivalóról. Mi gondosan válogatjuk össze a legjobb alapanyagokat, hogy Önnek csak az élvezetre kelljen koncentrálnia – az egészséges táplálkozás minden egyes falatban benne van. NutriGo: frissesség, ízek, energia – az Ön asztalára!</p>
-            <div className="footer-social-icons">
-                <img src={assets.facebook_icon} alt="" />
-                <img src={assets.twitter_icon} alt="" />
-                <img src={assets.linkedin_icon} alt="" />
-            </div>
+            
         </div>
-        <div className="footer-content-center">
-            <h2>COMPANY</h2>
-            <ul>
-                <li>Főoldal</li>
-                <li>Rólunk</li>
-                <li>Szállítás</li>
-                <li>Adatvédelem</li>
-            </ul>
-        </div>
+        
         <div className="footer-content-right">
             <h2>Kapcsolatfelvétel</h2>
             <ul>
